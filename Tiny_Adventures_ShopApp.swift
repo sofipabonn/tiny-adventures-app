cff917621ce011c9f721c98be5be42f6c55e128d
sofipabonn <sofip1012@gmail.com>
2026-05-04 23:56:26 -0500

    Add files via upload

---
+++ Tiny_Adventures_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tiny_Adventures_ShopApp.swift
+//  Tiny Adventures Shop
+//
+//  Created by Sofia on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Tiny_Adventures_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
